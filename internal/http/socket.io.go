688f4f156b49287aaa8c4a195fd9b497ab138ef8
DrEgg <dev@dregg.me>
2025-09-06 22:14:34 -0400

    Socket.io integration done

---
+++ internal/http/socket.io.go
@@ -0,0 +1,98 @@
+package http
+
+import (
+	"fmt"
+	"github.com/zishang520/socket.io/servers/engine/v3"
+	"github.com/zishang520/socket.io/servers/socket/v3"
+	"github.com/zishang520/socket.io/v3/pkg/types"
+	"net/http"
+)
+
+// Socket represents a wrapper around the Socket.IO server
+type Socket struct {
+	sock       *socket.Server
+	namespaces map[string]Namespace
+}
+
+// Initialize configures and creates the Socket.IO server
+func (self *Socket) Initialize() {
+	opts := socket.DefaultServerOptions()
+	opts.SetPath("/socket.io")
+	opts.SetTransports(types.NewSet(
+		engine.Polling,   // HTTP long-polling transport
+		engine.WebSocket, // WebSocket transport for real-time communication
+	))
+	self.sock = socket.NewServer(nil, opts)
+	self.namespaces = make(map[string]Namespace)
+}
+
+// AddNamespace creates a new Socket.IO namespace and adds it to the server
+func (self *Socket) AddNamespace(name string) {
+	namespace := Namespace{namespace: self.sock.Of(name, nil)}
+	namespace.Initialize()
+	self.namespaces[name] = namespace
+}
+
+// Handler returns an HTTP handler for the Socket.IO server
+func (self *Socket) Handler() http.Handler {
+	return self.sock.ServeHandler(nil)
+}
+
+// Namespace represents a Socket.IO namespace with custom event handling
+type Namespace struct {
+	namespace socket.Namespace
+	events    map[string]func(client *socket.Socket, data ...any)
+	//middileWare []func(client *socket.Socket, next func())
+}
+
+// Initialize sets up the namespace with default event handlers
+func (self *Namespace) Initialize() {
+	self.events = map[string]func(*socket.Socket, ...any){
+		"disconnect": func(client *socket.Socket, reason ...any) {},
+	}
+}
+
+// addEvent registers a custom event handler for the namespace
+func (self *Namespace) addEvent(event string, f func(*socket.Socket, ...any)) {
+	self.events[event] = f
+}
+
+// registerEvents activates all the event handlers for new client connections
+func (self *Namespace) registerEvents() {
+	self.namespace.On("connection", func(clients ...any) {
+		client := clients[0].(*socket.Socket)
+		for event, f := range self.events {
+			client.On(event, func(data ...any) { f(client, data) })
+		}
+	})
+}
+
+// addMiddleware adds a middleware to the namespace
+func (self *Namespace) addMiddleware(f func(client *socket.Socket, next func(*socket.ExtendedError))) {
+	self.namespace.Use(f)
+}
+
+func Start(addr string) error {
+	server := new(Socket)
+	server.Initialize()
+	server.AddNamespace("/ttt")
+
+	defaultNamespace := server.namespaces["/ttt"]
+
+	defaultNamespace.addEvent("message", func(client *socket.Socket, data ...any) {
+		client.Emit("message", data...)
+	})
+	defaultNamespace.registerEvents()
+
+	defaultNamespace.addMiddleware(func(client *socket.Socket, next func(*socket.ExtendedError)) {
+		fmt.Println(client.Handshake().Auth)
+		next(nil)
+	})
+	http.Handle("/socket.io/", server.Handler())
+
+	fmt.Println("Socket.IO服务器启动在: http://localhost:8080")
+	fmt.Println("访问测试页面: http://DrEggs-Mac-Pro-3.local:8080")
+	StartFrontend()
+
+	return http.ListenAndServe(":8080", nil)
+}
